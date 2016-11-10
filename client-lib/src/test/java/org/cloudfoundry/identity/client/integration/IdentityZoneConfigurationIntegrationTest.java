65c6f5f17ed7940f3ab0ac96f1105e837ddc8c39
Filip Hanik <fhanik@pivotal.io>
2016-11-10 15:11:54 -0800

    Add integration test case for new zone and its Cors configuration [#115118433] https://www.pivotaltracker.com/story/show/115118433

--- client-lib/src/test/java/org/cloudfoundry/identity/client/integration/IdentityZoneConfigurationIntegrationTest.java
+++ client-lib/src/test/java/org/cloudfoundry/identity/client/integration/IdentityZoneConfigurationIntegrationTest.java
@@ -20,7 +20,6 @@ import org.cloudfoundry.identity.client.token.GrantType;
 import org.cloudfoundry.identity.client.token.TokenRequest;
 import org.cloudfoundry.identity.uaa.zone.IdentityZone;
 import org.cloudfoundry.identity.uaa.zone.IdentityZoneConfiguration;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -29,9 +28,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.net.URI;
+import java.util.Arrays;
 
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.cloudfoundry.identity.client.integration.ClientIntegrationTestUtilities.GENERATOR;
 import static org.cloudfoundry.identity.client.integration.ClientIntegrationTestUtilities.UAA_URI;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.springframework.http.HttpHeaders.ACCEPT;
+import static org.springframework.http.HttpHeaders.ACCEPT_LANGUAGE;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.CONTENT_LANGUAGE;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 import static org.springframework.http.HttpMethod.POST;
 
 public class IdentityZoneConfigurationIntegrationTest {
@@ -40,6 +50,9 @@ public class IdentityZoneConfigurationIntegrationTest {
 
     @Rule
     public IsUAAListeningRule uaaListeningRule = new IsUAAListeningRule(UAA_URI, false);
+    private TokenRequest clientCredentials;
+    private UaaContext context;
+    private IdentityZone zone;
 
     @Before
     public void setUp() throws Exception {
@@ -47,23 +60,63 @@ public class IdentityZoneConfigurationIntegrationTest {
             UaaContextFactory.factory(new URI(UAA_URI))
                 .authorizePath("/oauth/authorize")
                 .tokenPath("/oauth/token");
-    }
 
-    @Test
-    public void create_zone_without_client_api() throws Exception {
-        TokenRequest clientCredentials = factory.tokenRequest()
+        clientCredentials = factory.tokenRequest()
             .setClientId("identity")
             .setClientSecret("identitysecret")
             .setGrantType(GrantType.CLIENT_CREDENTIALS);
 
-        UaaContext context = factory.authenticate(clientCredentials);
+        context = factory.authenticate(clientCredentials);
 
         String zoneId = GENERATOR.generate();
-        IdentityZone zone = new IdentityZone()
+        zone = new IdentityZone()
             .setId(zoneId)
             .setName(zoneId)
             .setSubdomain(zoneId)
             .setConfig(new IdentityZoneConfiguration());
+
+    }
+
+    @Test
+    public void create_zone_without_client_api() throws Exception {
+        ResponseEntity<IdentityZone> created = context.getRestTemplate().exchange(
+            UAA_URI+"/identity-zones",
+            POST,
+            new HttpEntity<>(zone),
+            IdentityZone.class
+        );
+
+        assertEquals(HttpStatus.CREATED, created.getStatusCode());
+
+    }
+
+    @Test
+    public void create_zone_with_default_cors_configuration() throws Exception {
+        ResponseEntity<IdentityZone> created = context.getRestTemplate().exchange(
+            UAA_URI+"/identity-zones",
+            POST,
+            new HttpEntity<>(zone),
+            IdentityZone.class
+        );
+
+        assertEquals(HttpStatus.CREATED, created.getStatusCode());
+        assertThat(created.getBody().getConfig().getCorsPolicy().getXhrConfiguration().getAllowedMethods(), containsInAnyOrder("GET"));
+        assertThat(created.getBody().getConfig().getCorsPolicy().getDefaultConfiguration().getAllowedMethods(), containsInAnyOrder("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        assertThat(created.getBody().getConfig().getCorsPolicy().getXhrConfiguration().getAllowedHeaders(), containsInAnyOrder(ACCEPT, ACCEPT_LANGUAGE, CONTENT_TYPE, CONTENT_LANGUAGE,AUTHORIZATION, "X-Requested-With"));
+        assertThat(created.getBody().getConfig().getCorsPolicy().getDefaultConfiguration().getAllowedHeaders(), containsInAnyOrder(ACCEPT, ACCEPT_LANGUAGE, CONTENT_TYPE, CONTENT_LANGUAGE,AUTHORIZATION));
+        assertTrue(created.getBody().getConfig().getCorsPolicy().getXhrConfiguration().isAllowedCredentials());
+        assertFalse(created.getBody().getConfig().getCorsPolicy().getDefaultConfiguration().isAllowedCredentials());
+
+    }
+
+    @Test
+    public void create_zone_with_cors_configuration() throws Exception {
+        zone.getConfig().getCorsPolicy().getDefaultConfiguration().setAllowedMethods(Arrays.asList("GET","POST","HEAD"));
+        zone.getConfig().getCorsPolicy().getXhrConfiguration().setAllowedMethods(Arrays.asList("GET","POST","OPTIONS"));
+        zone.getConfig().getCorsPolicy().getDefaultConfiguration().setAllowedHeaders(Arrays.asList(ACCEPT, ACCEPT_LANGUAGE, CONTENT_TYPE));
+        zone.getConfig().getCorsPolicy().getXhrConfiguration().setAllowedHeaders(Arrays.asList(ACCEPT, ACCEPT_LANGUAGE));
+        zone.getConfig().getCorsPolicy().getXhrConfiguration().setAllowedCredentials(false);
+        zone.getConfig().getCorsPolicy().getDefaultConfiguration().setAllowedCredentials(true);
 
         ResponseEntity<IdentityZone> created = context.getRestTemplate().exchange(
             UAA_URI+"/identity-zones",
@@ -72,7 +125,14 @@ public class IdentityZoneConfigurationIntegrationTest {
             IdentityZone.class
         );
 
-        Assert.assertEquals(HttpStatus.CREATED, created.getStatusCode());
+        assertEquals(HttpStatus.CREATED, created.getStatusCode());
+        assertThat(created.getBody().getConfig().getCorsPolicy().getXhrConfiguration().getAllowedMethods(), containsInAnyOrder("GET","POST","OPTIONS"));
+        assertThat(created.getBody().getConfig().getCorsPolicy().getDefaultConfiguration().getAllowedMethods(), containsInAnyOrder("GET","POST","HEAD"));
+        assertThat(created.getBody().getConfig().getCorsPolicy().getXhrConfiguration().getAllowedHeaders(), containsInAnyOrder(ACCEPT, ACCEPT_LANGUAGE));
+        assertThat(created.getBody().getConfig().getCorsPolicy().getDefaultConfiguration().getAllowedHeaders(), containsInAnyOrder(ACCEPT, ACCEPT_LANGUAGE, CONTENT_TYPE));
+        assertFalse(created.getBody().getConfig().getCorsPolicy().getXhrConfiguration().isAllowedCredentials());
+        assertTrue(created.getBody().getConfig().getCorsPolicy().getDefaultConfiguration().isAllowedCredentials());
 
     }
+
 }
